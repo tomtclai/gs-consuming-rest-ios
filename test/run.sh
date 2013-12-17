fa44d2c1a9843e2be6c1ee703808d3d73874a0ee
Roy Clarkson <rclarkson@gopivotal.com>
2013-12-17 10:40:58 -0600

    Update CI script

--- test/run.sh
+++ test/run.sh
@@ -1,35 +1,3 @@
 cd $(dirname $0)
 
-cd ../complete/server
-
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-    exit $ret
-fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-    exit $ret
-fi
-rm -rf build
-
-cd ../initial/server
-
-mvn clean package
-ret=$?
-if [ $ret -ne 0 ]; then
-    exit $ret
-fi
-rm -rf target
-
-./gradlew build
-ret=$?
-if [ $ret -ne 0 ]; then
-    exit $ret
-fi
-rm -rf build
-
-exit
+echo "iOS apps can only be built on OS X"
