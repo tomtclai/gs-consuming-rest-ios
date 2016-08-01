d3833f06400b3113add68dc7543068f804924d68
Lai, Tom <TszChun.Lai@nike.com>
2016-08-01 08:35:23 -0700

    Rm initial folder, as it is no longer needed

--- initial/Rest/RestViewController.h
+++
@@ -1,5 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@interface RestViewController : UIViewController
-
-@end
--- initial/Rest/main.m
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "RestAppDelegate.h"
-
-int main(int argc, char * argv[])
-{
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RestAppDelegate class]));
-    }
-}
